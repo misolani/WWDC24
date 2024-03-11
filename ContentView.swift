ae8edc4c3f0b8ac9f9eb5e95e6109590c0fc47de
laura misolani <lmisolani@gmail.com>
2024-03-11 09:49:15 -0300

    Adds Menu Page

--- ContentView.swift
+++ ContentView.swift
@@ -1,9 +1,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
+        
         NavigationStack {
-            MainView()
+            VStack {
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    NavigationLink{
+                        
+                    }label: {
+                        Text("licenses")
+                            .foregroundStyle(.black)
+                            .padding(.trailing)
+                    }
+                }
+                
+                VStack{
+                    
+                    Spacer()
+                    
+                    Text("Title")
+                        .font(.title)
+                        .padding(.bottom, 64)
+                    
+                    NavigationLink{
+                        MainView()
+                    }label: {
+                        Text("read story")
+                            .foregroundStyle(.black)
+                    }
+                    
+                    NavigationLink{
+                        
+                    }label: {
+                        Text("play")
+                            .foregroundStyle(.black)
+                    }
+                    
+                    Spacer()
+                }
+            }.background(Color("blue"))
         }
     }
 }
